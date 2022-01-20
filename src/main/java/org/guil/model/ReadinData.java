27610da4ab203a87c6f79d2551f74fa5166c743e
guil <g.bams@student.maastrichtuniversity.nl>
2022-01-20 13:01:59 +0100

    change way that file is read in

--- src/main/java/org/guil/model/ReadinData.java
+++ src/main/java/org/guil/model/ReadinData.java
@@ -36,23 +36,20 @@ public class ReadinData {
     public ReadinData() {
     }
 
-    private List<String[]> readAll(Reader reader) throws Exception {
-        CSVReader csvReader = new CSVReader(reader);
-        List<String[]> list = new ArrayList<>();
-        list = csvReader.readAll();
-        reader.close();
-        csvReader.close();
-        return list;
-    }
-
-
     public void readAll(File file) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            List<String[]> list = readAll(reader);
+            CSVReader csvReader = new CSVReader(reader);
+
+            List<String[]> list = csvReader.readAll();
+
+            csvReader.close();
+            reader.close();
+
             for(int i = 0; i<list.size(); i++){
                 System.out.println(Arrays.toString(list.get(i)));
             }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (Exception e) {
