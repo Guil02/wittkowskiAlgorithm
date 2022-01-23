6dab12837457da70e7ce350075bdb04f471000cc
guil <g.bams@student.maastrichtuniversity.nl>
2022-01-23 12:28:36 +0100

    fixed the multiThreading and added junit dependency in the pom.xml

--- src/main/java/org/guil/model/Wittkowski.java
+++ src/main/java/org/guil/model/Wittkowski.java
@@ -24,54 +24,59 @@
 
 package org.guil.model;
 
+import org.guil.utils.Functions;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Wittkowski {
     private List<String[]> list;
     private String[] firstRow;
     private final int amountOfProcessors;
+    private final int amountOfRows;
+    private ArrayList<Double> output;
+    private static final boolean temp = false;
+
     public Wittkowski(List<String[]> list) {
         firstRow = list.get(0);
         list.remove(0);
         this.list = list;
+
         amountOfProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println(amountOfProcessors + " processors available");
+        amountOfRows = list.size();
+        intializeOutputList();
     }
 
     public void run() {
-        int sampleSize = list.size();
-        ExecutorService executorService = Executors.newFixedThreadPool(amountOfProcessors);
-        List<Future> tasks = new ArrayList<>();
-
-        for (int offset = 0; offset < amountOfProcessors; offset++) {
-
-            int finalParallelism = amountOfProcessors;
-            int finalOffset = offset;
-            Future task = executorService.submit(() -> {
-                int i = finalOffset;
-                while (list.size() > i) {
-                    processItem(list.get(i));
-                    i += finalParallelism;
-                }
-            });
-            tasks.add(task);
+        List<List<String[]>> partition = Functions.partitionList(list, amountOfProcessors);
+        ProcessingThread[] task = new ProcessingThread[amountOfProcessors];
+        Thread[] threads = new Thread[amountOfProcessors];
+        for(int i = 0; i<amountOfProcessors; i++){
+            task[i] = new ProcessingThread(partition.get(i), this);
+            threads[i] = new Thread(task[i]);
+            threads[i].start();
         }
-        for (Future task : tasks) {
+        for(int i = 0; i <task.length; i++){
             try {
-                task.get();
-            } catch (InterruptedException | ExecutionException e) {
+                threads[i].join();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
-        executorService.shutdown();
+        System.out.println(list.size());
     }
-    private void processItem(String[] strings) {
-        System.out.println(Arrays.toString(strings));
+
+
+    private void intializeOutputList() {
+       output = new ArrayList<>();
+       for(int i = 0; i<amountOfRows; i++){
+           output.add(0.0);
+       }
+    }
+    public void processItem(String[] strings) {
+//        System.out.println(Arrays.toString(strings));
     }
 }
